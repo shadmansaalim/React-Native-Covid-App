7390082d8f15b534d0e95f137e5552a3dd814449
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-18 01:52:15 +1000

    Footer component

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { NativeBaseProvider, Box, Text, Heading, VStack, FormControl, Input, Link, Button, Icon, HStack, Center, Pressable } from "native-base";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
-import { FontAwesome } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+
 const Footer = () => {
-    const [selected, setSelected] = useState(1);
+    const [selected, setSelected] = useState(0);
     return (
         <NativeBaseProvider>
             <Box flex={1} bg="white" safeAreaTop width="100%" maxW="100%" alignSelf="center">
@@ -11,58 +14,25 @@ const Footer = () => {
                 <HStack bg="dark.50" alignItems="center" safeAreaBottom shadow={6}>
                     <Pressable opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
                         <Center>
-                            <Icon mb="1" as={<FontAwesome name="flag" size={24} color="black" />}
-                                color=
-                                {selected == 0
-                                    ?
-                                    "darkBlue.500"
-                                    :
-                                    "white"
-                                } size="sm" />
-                            <Text color=
-                                {selected == 0
-                                    ?
-                                    "darkBlue.500"
-                                    :
-                                    "white"
-                                } fontSize="12">
+                            <Icon mb="1" as={<MaterialCommunityIcons name={selected === 0 ? "home" : "home-outline"} />} color="white" size="sm" />
+                            <Text color="white" fontSize="12">
                                 Home
                             </Text>
                         </Center>
                     </Pressable>
                     <Pressable opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
                         <Center>
-                            <Icon mb="1" as={<MaterialIcons name="search" />} color={selected == 0
-                                ?
-                                "darkBlue.500"
-                                :
-                                "white"
-                            } size="sm" />
-                            <Text color={selected == 0
-                                ?
-                                "darkBlue.500"
-                                :
-                                "white"
-                            } fontSize="12">
-                                Search
+                            <Icon mb="1" as={<Feather name="flag" size={24} color="black" />} color="white" size="sm" />
+                            <Text color="white" fontSize="12">
+                                Countries
                             </Text>
                         </Center>
                     </Pressable>
                     <Pressable opacity={selected === 2 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(2)}>
                         <Center>
-                            <Icon mb="1" as={<MaterialCommunityIcons name={selected === 2 ? "cart" : "cart-outline"} />} color={selected == 0
-                                ?
-                                "darkBlue.500"
-                                :
-                                "white"
-                            } size="sm" />
-                            <Text color={selected == 0
-                                ?
-                                "darkBlue.500"
-                                :
-                                "white"
-                            } fontSize="12">
-                                Cart
+                            <Icon mb="1" as={<AntDesign name="infocirlceo" size={24} color="black" />} color="white" size="sm" />
+                            <Text color="white" fontSize="12">
+                                About
                             </Text>
                         </Center>
                     </Pressable>
