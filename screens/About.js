7ff1591dff23293d0fee237cea2496afa99c370a
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-18 01:55:56 +1000

    Navigation package installed and screens set

---
+++ screens/About.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+const About = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default About;
