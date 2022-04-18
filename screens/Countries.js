6fb8e6113f9407371faa6fc24717d49ba91af551
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-19 04:16:11 +1000

    Spinner added for better user experience

--- screens/Countries.js
+++ screens/Countries.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import { View, VStack, Input, Icon, Text, FlatList, ScrollView, Image } from 'native-base';
+import { View, VStack, Input, Icon, Text, ScrollView, Image, HStack, Spinner } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 import { globalStyles } from '../styles/global';
 import CountryData from '../components/CountryData';
@@ -8,7 +8,6 @@ import CountryData from '../components/CountryData';
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     const [displayCountries, setDisplayCountries] = useState([]);
-    const [click, setClick] = useState(false);
     const [searchText, setSearchText] = useState('');
     useEffect(() => {
         fetch('https://coronavirus-19-api.herokuapp.com/countries/')
@@ -22,59 +21,52 @@ const Countries = () => {
 
     const handleSearch = text => {
         setSearchText(text);
-        setClick(true);
         const userCountries = countries.filter(country => country.country.toLowerCase().includes(text.toLowerCase()));
         setDisplayCountries(userCountries);
     }
     return (
-        <ScrollView style={styles.container}>
-            <VStack w="100%" alignSelf="center" padding={6}>
-                <Input
-                    value={searchText}
-                    onChangeText={text => handleSearch(text)}
-                    color="white" placeholder="Search your country" width="100%" borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
-            </VStack>
-
+        <>
             {
-                click
+                countries.length
                     ?
-                    (
-                        displayCountries.length
-                            ?
-                            <View>
-                                <Text textAlign="center" marginTop={4} marginBottom={6} style={globalStyles.titleText}>Live Covid-19 Information</Text>
-                                {
-                                    displayCountries.map(country => <CountryData
-                                        key={country.country}
-                                        country={country}
-                                    ></CountryData>)
-                                }
-                            </View>
-                            :
-                            <View style={styles.searchNotFound}>
-                                <Text marginTop={4} marginBottom={6} style={globalStyles.titleText} color="white">Search Result Not Found</Text>
-                                <Image
-                                    source={require('../assets/searchNotFound.png')}
-                                    size="96"
-                                    alt="Search Not Found Image"
-                                />
-                            </View>
+                    <ScrollView style={styles.container}>
+                        <VStack w="100%" alignSelf="center" padding={6}>
+                            <Input
+                                value={searchText}
+                                onChangeText={text => handleSearch(text)}
+                                color="white" placeholder="Search your country" width="100%" borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
+                        </VStack>
 
-                    )
-                    :
-                    <View>
-                        <Text textAlign="center" marginTop={4} marginBottom={6} style={globalStyles.titleText}>Live Covid-19 Information</Text>
                         {
-                            countries.map(country =>
-                                <CountryData
-                                    key={country.country}
-                                    country={country}
-                                ></CountryData>)
+                            displayCountries.length
+                                ?
+                                <View>
+                                    <Text textAlign="center" marginTop={4} marginBottom={6} style={globalStyles.titleText}>Live Covid-19 Information</Text>
+                                    {
+                                        displayCountries.map(country => <CountryData
+                                            key={country.country}
+                                            country={country}
+                                        ></CountryData>)
+                                    }
+                                </View>
+                                :
+                                <View style={styles.searchNotFound}>
+                                    <Text marginTop={4} marginBottom={6} style={globalStyles.titleText} color="white">Search Result Not Found</Text>
+                                    <Image
+                                        source={require('../assets/searchNotFound.png')}
+                                        size="96"
+                                        alt="Search Not Found Image"
+                                    />
+                                </View>
                         }
-                    </View>
-            }
 
-        </ScrollView>
+                    </ScrollView>
+                    :
+                    <HStack justifyContent="center" alignItems="center" height="100%">
+                        <Spinner size="lg" color="dark.600" />
+                    </HStack>
+            }
+        </>
 
     );
 };
