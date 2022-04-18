2cc0eee0ae53694ef07105cb4926488cd0ae43b7
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-18 23:34:26 +1000

    Search bar added for countries page

--- screens/Countries.js
+++ screens/Countries.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
-
+import { View, VStack, Input, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { globalStyles } from '../styles/global';
 
 const Countries = () => {
     return (
-        <>
+        <View style={globalStyles.container}>
+            <VStack w="100%" alignSelf="center">
+                <Input color="white" placeholder="Search your country" width="100%" borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
+            </VStack>
+        </View>
 
-
-        </>
     );
 };
 
